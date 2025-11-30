f71eae96f2d7d4256242d50297f7672d3d13886d
Ikhwanulrizki854 <ikhwanulrizki765@gmail.com>
2025-11-30 09:31:19 +0700

    Force IPv4 for email

--- Server.js
+++ Server.js
@@ -60,13 +60,12 @@ app.get('/test-users', (req, res) => {
 
 // KONFIGURASI NODEMAILER
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 587,              
-  secure: false,         
+  service: 'gmail', 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-  }
+  },
+  family: 4
 });
 
 // Fungsi Helper untuk Kirim Email
