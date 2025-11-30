2bc83f9a8eff53512e5706067ef15559f0c26f81
Ikhwanulrizki854 <ikhwanulrizki765@gmail.com>
2025-11-30 08:49:31 +0700

    Ganti ke Port 587

--- Server.js
+++ Server.js
@@ -60,12 +60,14 @@ app.get('/test-users', (req, res) => {
 
 // KONFIGURASI NODEMAILER
 const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com', 
-  port: 465,              
-  secure: true,           
+  host: 'smtp.gmail.com',
+  port: 587,              
+  secure: false,         
+  auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-  });
+  }
+});
 
 // Fungsi Helper untuk Kirim Email
 const sendEmail = (to, subject, htmlContent) => {
